125034ebc223e133b39257f0c94bde21dacbf2ab
Juan Lulkin <maiz@lulk.in>
2016-07-05 18:51:36 +0200

    Removes bs v2

--- src/decorators/scatter-or-render.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Scatter from '../components/scatter'
-
-export default (Component, name = Component.displayName || Component.name) => (props) => (
-  props.adapter
-    ? <Scatter name={name} adapter={props.adapter} props={props} />
-    : <Component {...props} />
-)
