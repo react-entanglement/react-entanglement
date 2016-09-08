b053af70f949ae0da68730ca95f6679e44a6f049
Paulo Ragonha <paulo@ragonha.me>
2016-09-08 19:44:54 +0200

    Fix unit tests for the exposed API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import Entanglement from './components/entanglement'
-import Materialize from './components/materialize'
+import materialize from './components/materialize'
 import passthroughAdapter from './adapters/passthrough-adapter'
 import scatter from './components/scatter'
 
-Entanglement.Materialize = Materialize
+Entanglement.materialize = materialize
 Entanglement.passthroughAdapter = passthroughAdapter
 Entanglement.scatter = scatter
 
