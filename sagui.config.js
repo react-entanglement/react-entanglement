3112c2dd9ea6e932efe17f25e4585d99bb102c31
Xavier Via <fernando.via@gmail.com>
2017-10-31 16:48:33 +0100

    Remove Enzyme specific Sagui config

--- sagui.config.js
+++ sagui.config.js
@@ -8,14 +8,4 @@ module.exports = {
   style: {
     cssModules: false,
   },
-
-  additionalWebpackConfig: {
-    // Enzyme setup
-    externals: {
-      cheerio: 'window',
-      'react/addons': true,
-      'react/lib/ExecutionEnvironment': true,
-      'react/lib/ReactContext': true,
-    },
-  },
 }
