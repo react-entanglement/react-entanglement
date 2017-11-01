844a7aa2d0492fe03e4641c9fffc72a8bcc882a2
Xavier Via <fernando.via@gmail.com>
2017-11-01 11:17:41 +0100

    Buffer calls to render in passthroughAdapter

--- src/adapters/passthrough-adapter.js
+++ src/adapters/passthrough-adapter.js
@@ -1,38 +1,46 @@
 import EventEmitter from 'events'
 
 export default () => {
-  const emiter = new EventEmitter()
+  const emitter = new EventEmitter()
+
+  const buffer = {}
 
   return {
     scatterer: {
-      unmount: componentName => emiter.emit(`unmount:${componentName}`),
+      unmount: componentName => {
+        emitter.emit(`unmount:${componentName}`)
+      },
 
       render: (componentName, data, handlerNames, context) => {
-        emiter.emit(`render:${componentName}`, data, handlerNames, context)
+        emitter.emit(`render:${componentName}`, data, handlerNames, context)
+        buffer[`render:${componentName}`] = [data, handlerNames, context]
       },
 
       addHandlerListener: (componentName, handlerName, cb) => {
         const eventName = `handle:${componentName}:${handlerName}`
-        emiter.on(eventName, cb)
-        return () => emiter.removeListener(eventName, cb)
+        emitter.on(eventName, cb)
+        return () => emitter.removeListener(eventName, cb)
       },
     },
 
     materializer: {
       addUnmountListener: (componentName, cb) => {
         const eventName = `unmount:${componentName}`
-        emiter.on(eventName, cb)
-        return () => emiter.removeListener(eventName, cb)
+        emitter.on(eventName, cb)
+        return () => emitter.removeListener(eventName, cb)
       },
 
       addRenderListener: (componentName, cb) => {
         const eventName = `render:${componentName}`
-        emiter.on(eventName, cb)
-        return () => emiter.removeListener(eventName, cb)
+        emitter.on(eventName, cb)
+        if (buffer[`render:${componentName}`]) {
+          cb(...buffer[`render:${componentName}`]) // eslint-disable-line standard/no-callback-literal
+        }
+        return () => emitter.removeListener(eventName, cb)
       },
 
       handle: (componentName, handlerName, args) =>
-        emiter.emit(`handle:${componentName}:${handlerName}`, args),
+        emitter.emit(`handle:${componentName}:${handlerName}`, args),
     },
   }
 }
